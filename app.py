a861dd613aa19dfe56412ebb1711adac244dbefb
Carlos Carranza <carloscarranza2486@gmail.com>
2026-05-21 18:33:49 -0500

    fix: path file refactor

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 # Load the dataset
-car_data = pd.read_csv("../vehicles_us.csv")
+car_data = pd.read_csv("./vehicles_us.csv")
 
 
 st.header(
